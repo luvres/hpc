f562883b9fe7ce0aeaea98c1c244abd15e2f5588
luvres <luvres@hotmail.com>
2022-12-26 19:03:10 -0300

    Update cluster

--- wwhpc.sh
+++ wwhpc.sh
@@ -344,6 +344,7 @@ function addnodes() {
 
 	wwctl node delete $ood --yes &>/dev/null
 	wwctl node add $ood
+	wwctl node add xdmod
 
 	# Config nodes
 	#wwctl node set cn81 -n default -N eth0 -M 255.255.255.240 -I 40.6.18.81 -H fa:ce:40:06:18:81 -R generic,chrony,slurm -C r8-nv-slurm --yes
