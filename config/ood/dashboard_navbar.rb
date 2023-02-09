0316705ce4b795ce424fbab8e66a0d2e37d08c31
luvres <luvres@hotmail.com>
2023-02-09 18:25:35 -0300

    Mount /scratch on ood and show file manager

--- config/ood/dashboard_navbar.rb
+++ config/ood/dashboard_navbar.rb
@@ -1,3 +1,8 @@
 NavConfig.categories_whitelist=true
 NavConfig.categories=["Files", "Jobs", "Clusters", "Interactive Apps"]
 #NavConfig.categories=["Apps","Files", "Jobs", "Clusters", "Interactive Apps", "Reports"]
+
+OodFilesApp.candidate_favorite_paths.tap do |paths|
+  # add scratch space directories
+  paths << FavoritePath.new("/scratch/#{User.new.name}", title: "Scratch")
+end
